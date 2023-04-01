bfb336c0fe5c9a1df5f301989290febd91e79d75
An Luong <anpluong@gmail.com>
2023-04-01 01:04:43 -0500

    Completed fitlering, form, and embbedded map

--- src/Expenses/Expenses.js
+++ src/Expenses/Expenses.js
@@ -1,15 +1,31 @@
-import ExpenseItem from "./ExpenseItem";
-import Card from "../UI/Card";
-import "./Expenses.css";
+import React, { useState } from 'react';
+
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import './Expenses.css';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
-    <Card className="expenses">
-      {/* This is the way for embedded map */}
-      {props.items.map((item) => {
-        return <ExpenseItem key={item.id} title={item.title} amount={item.amount} />;
-      })}
-    </Card>
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
   );
 };
 
